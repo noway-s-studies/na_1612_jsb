51899aabf57e7728a9d41d7743955910219c4898
noway0032@gmail.com <noway0032@gmail.com>
2018-07-10 19:14:50 +0200

    start

---
+++ SecretSanta/src/main/java/secretsanta/SecretSantaApp.java
@@ -0,0 +1,20 @@
+package secretsanta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class SecretSantaApp extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(SecretSantaApp.class);
+    }
+    
+    public static void main(String... args) {
+        SpringApplication.run(SecretSantaApp.class, args);
+    }
+    
+}
---
+++ SecretSanta/src/main/resources/META-INF/resources/WEB-INF/jsp/createGroup.jsp
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<%@ taglib prefix="spring" uri="http://www.springframework.org/tags"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<html lang="en">
+    <head>
+        <title>SecretSanta</title>
+    </head>
+    <body>
+        <div>
+            <h1>SecretSanta</h1>
+        </div>
+        <div id="container">
+            <h1>${message}</h1>
+        </div>
+    </body>
+
+</html>
